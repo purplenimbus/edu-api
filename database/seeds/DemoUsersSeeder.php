77fcb65c835f99440acf0e3bed22b7ec13f32fbb
purplenimbus <you@example.com>
2018-08-27 21:41:39 -0700

    dont seed dummy users in staging

--- database/seeds/DemoUsersSeeder.php
+++ database/seeds/DemoUsersSeeder.php
@@ -36,7 +36,7 @@ class DemoUsersSeeder extends Seeder
 					'password'	=>	app('hash')->make('easier'),
 				])->each(function($superadmin)use($count){ $count++; });
 
-				$demoTeacher 	=	factory(App\User::class,'teacher',1)->create([
+				/*$demoTeacher 	=	factory(App\User::class,'teacher',1)->create([
 					'tenant_id' => $tenant->id,
 					'image' =>	'https://www.victoria147.com/wp-content/uploads/2014/10/user-avatar-placeholder.png',
 					'firstname'		=>	'demo',
@@ -53,7 +53,7 @@ class DemoUsersSeeder extends Seeder
 							'email'		=>	'student@yopmail.com',
 							'password'	=>	app('hash')->make('demo'),
 							'meta' => ['course_grade_id' => 1]
-						])->each(function($student)use($count){ $count++; });
+						])->each(function($student)use($count){ $count++; });*/
 		});
 
 		\Log::info('Created '.$count.' total test users');
