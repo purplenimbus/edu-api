17ce4cf4fc7b46ebc33b9457ed4829f04c5a12cb
purplenimbus <you@example.com>
2020-04-10 22:20:05 -0700

    clean up code

--- database/seeds/PermissionsSeeder.php
+++ database/seeds/PermissionsSeeder.php
@@ -23,7 +23,7 @@ class PermissionsSeeder extends Seeder
           'tenants' => ['view', 'edit'],
         ],
       ],
-    	[
+      [
         'name' => 'alumni',
         'permissions' => [
           'bills' => ['view'],
@@ -31,18 +31,18 @@ class PermissionsSeeder extends Seeder
           'registrations' => ['view'],
         ],
       ],
-    	[
+      [
         'name' => 'other',
         'permissions' => [],
       ],
-    	[
+      [
         'name' => 'student',
         'permissions' => [
           'courses' => ['view'],
           'registrations' => ['view'],
         ]
       ],
-    	[
+      [
         'name' => 'superadmin',
         'permissions' => [
           'bills' => ['view', 'edit', 'delete'],
@@ -60,7 +60,7 @@ class PermissionsSeeder extends Seeder
           'registrations' => ['view'],
         ],
       ],
-    	[
+      [
         'name' => 'parent',
         'permissions' => [
           'bills' => ['view'],
@@ -70,69 +70,27 @@ class PermissionsSeeder extends Seeder
       ],
     ];
 
-    $this->create_roles($roles);
-
     $permissions = [
-    	'bills' => ['create','view','edit','delete'],
-    	'courses' => ['create','view','edit','delete'],
-    	'users' => ['create','view','edit','delete'],
+      'bills' => ['create','view','edit','delete'],
+      'courses' => ['create','view','edit','delete'],
+      'users' => ['create','view','edit','delete'],
       'registrations' => ['create','view','edit','delete'],
-    	'tenants' => ['create','view','edit','delete'],
-		];
+      'tenants' => ['create','view','edit','delete'],
+    ];
 
     $this->create_permissions($permissions);
 
-    $permission_mappings = [
-      'student' => [
-        'courses' => ['view'],
-        'registrations' => ['view'],
-      ],
-      'instructor' => [
-        'courses' => ['view', 'edit'],
-        'users' => ['view'],
-        'registrations' => ['view'],
-      ],
-      'parent' => [
-        'bills' => ['view'],
-        'courses' => ['view'],
-        'registrations' => ['view'],
-      ],
-      'admin' => [
-        'bills' => ['view', 'edit', 'delete'],
-        'courses' => ['view', 'edit', 'delete'],
-        'users' => ['view', 'edit', 'delete'],
-        'registrations' => ['view', 'edit', 'delete'],
-        'tenants' => ['view', 'edit'],
-      ],
-      'superadmin' => [
-        'bills' => ['view', 'edit', 'delete'],
-        'courses' => ['view', 'edit', 'delete'],
-        'users' => ['view', 'edit', 'delete'],
-        'registrations' => ['view', 'edit', 'delete'],
-        'tenants' => ['view', 'edit', 'delete'],
-      ],
-      'alumni' => [
-        'bills' => ['view'],
-        'users' => ['view'],
-        'registrations' => ['view'],
-      ],
-    ];
-
-    $this->map_roles($roles);
+    $this->create_roles($roles);
   }
 
   private function create_roles($roles) {
     foreach ($roles as $role) {
-      $role = Bouncer::role()->firstOrCreate(
+      Bouncer::role()->firstOrCreate(
         Arr::only($role, ['name'])
       );
-    }
-  }
 
-  private function map_roles($roles) {
-    foreach ($roles as $role) {
       if (isset($role['permissions'])) {
-         foreach ($role['permissions'] as $resource_key => $permissions) {
+        foreach ($role['permissions'] as $resource_key => $permissions) {
           foreach ($permissions as $permission) {
             Bouncer::allow($role['name'])->to("{$permission}-{$resource_key}");         
           }
