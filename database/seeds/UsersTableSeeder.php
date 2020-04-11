af3b93e0155ec63f198cc570e2b1a090b1ab52b9
purplenimbus <you@example.com>
2020-04-10 23:18:48 -0700

    fix indentation

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,23 +18,23 @@ class UsersTableSeeder extends Seeder
     $nimbus_edu = new NimbusEdu(Tenant::find(1));
 
     $course_grades = [7,8,9,10,11,12,13];
-	
-		foreach($course_grades as $course_grade){
-			$students = factory(App\Student::class, 'student', 10)
-				->create([
-					'tenant_id' => $nimbus_edu->tenant->id,
-					'meta' => [
-						'course_grade_id' => $course_grade
-					]
-				])
-				->each(function($student) use ($nimbus_edu, $course_grade, $count){
-					$nimbus_edu->enrollCoreCourses($student, $course_grade);
+  
+    foreach($course_grades as $course_grade){
+      $students = factory(App\Student::class, 'student', 10)
+        ->create([
+          'tenant_id' => $nimbus_edu->tenant->id,
+          'meta' => [
+            'course_grade_id' => $course_grade
+          ]
+        ])
+        ->each(function($student) use ($nimbus_edu, $course_grade, $count){
+          $nimbus_edu->enrollCoreCourses($student, $course_grade);
 
-					$count++;
-				});
-			
-			\Log::info('Created '.$students->count().' students for course_grade '.$course_grade);
-			var_dump('Created '.$students->count().' students for course_grade '.$course_grade);
-		}
-	}
+          $count++;
+        });
+      
+      \Log::info('Created '.$students->count().' students for course_grade '.$course_grade);
+      var_dump('Created '.$students->count().' students for course_grade '.$course_grade);
+    }
+  }
 }
