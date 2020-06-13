a0311de2b0f2f6ea426f7550372ca2d364ba2fe7
purplenimbus <you@example.com>
2020-06-13 12:39:55 -0700

    reindent

--- app/NimbusEdu/Syllabus.php
+++ app/NimbusEdu/Syllabus.php
@@ -22,43 +22,43 @@ class Syllabus
   }
 
   public function processCurriculum($course_load, $payload) {
-  	$this->payload = $payload;
+    $this->payload = $payload;
 
-  	$course_grade_id = $course_load['course_grade_id'];
+    $course_grade_id = $course_load['course_grade_id'];
 
-  	if ($course_grade_id) {
+    if ($course_grade_id) {
       $curriculum = Curriculum::firstOrCreate([
         'course_grade_id' => $course_grade_id,
         'type_id' => $this->curriculum_type->id,
       ]);
 
-    	if(isset($course_load['core_subjects_code'])) {
-      	$this->processSubjects(
-      		$course_load['core_subjects_code'],
-      		$course_grade_id,
-      		'core',
-      		$curriculum,
-      	);
+      if(isset($course_load['core_subjects_code'])) {
+        $this->processSubjects(
+          $course_load['core_subjects_code'],
+          $course_grade_id,
+          'core',
+          $curriculum,
+        );
       }
 
       if(isset($course_load['elective_subjects_code'])) {
-      	$this->processSubjects(
-      		$course_load['elective_subjects_code'],
-      		$course_grade_id,
-      		'elective',
-      		$curriculum,
-      	);
+        $this->processSubjects(
+          $course_load['elective_subjects_code'],
+          $course_grade_id,
+          'elective',
+          $curriculum,
+        );
       }
 
       if(isset($course_load['optional_subjects_code'])) {
-      	$this->processSubjects(
-      		$course_load['optional_subjects_code'],
-      		$course_grade_id,
-      		'optional',
-      		$curriculum,
-      	);
+        $this->processSubjects(
+          $course_load['optional_subjects_code'],
+          $course_grade_id,
+          'optional',
+          $curriculum,
+        );
       }
-  	}
+    }
 
     return $this->payload;
   }
@@ -70,25 +70,25 @@ class Syllabus
   }
 
   private function processSubjects(
-  	$data,
-  	$course_grade_id,
-  	$type,
-  	Curriculum $curriculum){
+    $data,
+    $course_grade_id,
+    $type,
+    Curriculum $curriculum){
     $core_subjects_codes = explode(',', $data);
 
     foreach ($core_subjects_codes as $code) {
-    	$subject = $this->getSubject($code);
-    	$curriculum_course_load_type = $this->getCurriculumCourseLoadType($type);
+      $subject = $this->getSubject($code);
+      $curriculum_course_load_type = $this->getCurriculumCourseLoadType($type);
 
-    	if ($subject && $curriculum_course_load_type) {
-    		$this->payload['created'][] = $curriculum
-    			->subjects()
-    			->firstOrCreate([
-	    			'subject_id' => $subject->id,
-	    			'type_id' => $curriculum_course_load_type->id
-	    		])
-	    		->toArray();
-    	}
+      if ($subject && $curriculum_course_load_type) {
+        $this->payload['created'][] = $curriculum
+          ->subjects()
+          ->firstOrCreate([
+            'subject_id' => $subject->id,
+            'type_id' => $curriculum_course_load_type->id
+          ])
+          ->toArray();
+      }
     }
 
     return $this->payload;
@@ -96,11 +96,11 @@ class Syllabus
 
   public function getCurriculumCourseLoadType($name){
     return CurriculumCourseLoadType::where('name', $name)
-    	->first();
+      ->first();
   }
 
   private function getSubject($code){
     return Subject::where('code', $code)
-    	->first();
+      ->first();
   }
 }
--- database/seeds/DemoUsersSeeder.php
+++ database/seeds/DemoUsersSeeder.php
@@ -12,41 +12,41 @@ class DemoUsersSeeder extends Seeder
      */
     public function run()
     {
-			$count = 0;
-			$records = factory(App\Tenant::class, 1)
-			->create([
-				'country' => config('edu.default.country'),
-				'name'=> 'showers christian high school',
-			])
-			->each(function($tenant)use($count){
-					$institution = new Institution($tenant);
-					$institution->generateSubjects();
-					$institution->generateClasses();
-					$institution->generateCurriculum();
+      $count = 0;
+      $records = factory(App\Tenant::class, 1)
+      ->create([
+        'country' => config('edu.default.country'),
+        'name'=> 'showers christian high school',
+      ])
+      ->each(function($tenant)use($count){
+          $institution = new Institution($tenant);
+          $institution->generateSubjects();
+          $institution->generateClasses();
+          $institution->generateCurriculum();
 
-					factory(App\User::class, 1)->create([
-						'tenant_id' => $tenant->id,
-						'firstname'		=>	'ekama',
-						'lastname'		=>	'akpan',
-						'email'		=>	'info@showersedu.com',
-						'password'	=>	app('hash')->make('info@showersedu.com'),
-					])->each(function($admin)use($count){ 
-						$count++; 
-						$admin->assign('admin');
-					});
+          factory(App\User::class, 1)->create([
+            'tenant_id' => $tenant->id,
+            'firstname'   =>  'ekama',
+            'lastname'    =>  'akpan',
+            'email'   =>  'info@showersedu.com',
+            'password'  =>  app('hash')->make('info@showersedu.com'),
+          ])->each(function($admin)use($count){ 
+            $count++; 
+            $admin->assign('admin');
+          });
 
-					factory(App\User::class, 1)->create([
-						'tenant_id' => $tenant->id,
-						'firstname'		=>	'anthony',
-						'lastname'		=>	'akpan',
-						'email'		=>	'anthony.akpan@hotmail.com',
-						'password'	=>	app('hash')->make('easier'),
-					])->each(function($superadmin)use($count){ 
-						$count++;
-						$superadmin->assign('superadmin');
-					});
-			});
+          factory(App\User::class, 1)->create([
+            'tenant_id' => $tenant->id,
+            'firstname'   =>  'anthony',
+            'lastname'    =>  'akpan',
+            'email'   =>  'anthony.akpan@hotmail.com',
+            'password'  =>  app('hash')->make('easier'),
+          ])->each(function($superadmin)use($count){ 
+            $count++;
+            $superadmin->assign('superadmin');
+          });
+      });
 
-		\Log::info('Created '.$count.' total test users');
+    \Log::info('Created '.$count.' total test users');
   }
 }
