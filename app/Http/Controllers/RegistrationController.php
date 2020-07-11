a15b876d704278249f9e322bf223d345e13ecec1
purplenimbus <you@example.com>
2020-07-10 21:44:09 -0700

    fix tabs

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -16,96 +16,96 @@ use App\Nimbus\Enrollment;
 
 class RegistrationController extends Controller
 {
-  /**
-   * List registrations
-   *
-   * @return void
-   */
-  public function index(Request $request)
-  {
-    $tenant_id = Auth::user()->tenant()->first()->id;
+	/**
+	 * List registrations
+	 *
+	 * @return void
+	 */
+	public function index(Request $request)
+	{
+		$tenant_id = Auth::user()->tenant()->first()->id;
 
-    $registrations = QueryBuilder::for(Registration::class)
-      ->defaultSort('created_at')
-      ->allowedSorts(
-        'created_at',
-        'updated_at'
-      )
-      ->allowedFilters([
-        AllowedFilter::callback('user_id', function (Builder $query, $value) {
-            return $query->where('user_id', '=', (int)$value);
-        }),
-        AllowedFilter::callback('course_id', function (Builder $query, $value) {
-            return $query->where('course_id', '=', (int)$value);
-        }),
-      ])
-      ->allowedFields(
-        'course.grade',
-        'course.instructor.id',
-        'course.instructor.name',
-        'course.subject',
-        'course_score',
-        'user.id',
-        'user.firstname',
-        'user.lastname',
-        'user.othernames',
-      )
-      ->allowedIncludes([
-        'course',
-        'course.instructor',
-        'course_score',
-        'course.grade',
-        'course.subject',
-        'course.status',
-        'score',
-        'term',
-        'user',
-      ])
-      ->where([
-        ['tenant_id', '=', $tenant_id]
-      ])
-      ->paginate($request->paginate ?? config('edu.pagination'));
-    
-    return response()->json($registrations, 200);
-  }
+		$registrations = QueryBuilder::for(Registration::class)
+			->defaultSort('created_at')
+			->allowedSorts(
+				'created_at',
+				'updated_at'
+			)
+			->allowedFilters([
+				AllowedFilter::callback('user_id', function (Builder $query, $value) {
+						return $query->where('user_id', '=', (int)$value);
+				}),
+				AllowedFilter::callback('course_id', function (Builder $query, $value) {
+						return $query->where('course_id', '=', (int)$value);
+				}),
+			])
+			->allowedFields(
+				'course.grade',
+				'course.instructor.id',
+				'course.instructor.name',
+				'course.subject',
+				'course_score',
+				'user.id',
+				'user.firstname',
+				'user.lastname',
+				'user.othernames',
+			)
+			->allowedIncludes([
+				'course',
+				'course.instructor',
+				'course_score',
+				'course.grade',
+				'course.subject',
+				'course.status',
+				'score',
+				'term',
+				'user',
+			])
+			->where([
+				['tenant_id', '=', $tenant_id]
+			])
+			->paginate($request->paginate ?? config('edu.pagination'));
+		
+		return response()->json($registrations, 200);
+	}
 
-  /**
-   * Register many students in many courses
-   *
-   * @return void
-   */
-  public function batch(RegisterStudent $request){
-    $tenant = Auth::user()->tenant()->first();
+	/**
+	 * Register many students in many courses
+	 *
+	 * @return void
+	 */
+	public function batch(RegisterStudent $request){
+		$tenant = Auth::user()->tenant()->first();
 
-    $enrollmentService = new Enrollment($tenant);
+		$enrollmentService = new Enrollment($tenant);
 
-    $registrations = $enrollmentService
-      ->enrollStudents($request->student_ids, $request->course_ids);
+		$registrations = $enrollmentService
+			->enrollStudents($request->student_ids, $request->course_ids);
 
-    return response()->json($registrations, 200);
-  }
+		return response()->json($registrations, 200);
+	}
 
-  /**
-   * Update scores
-   *
-   * @return void
-   */
-  public function update_scores(UpdateScores $request){
-    $registration = Registration::with('score')->find($request->id);
+	/**
+	 * Update scores
+	 *
+	 * @return void
+	 */
+	public function update_scores(UpdateScores $request){
+		$registration = Registration::with('score')->find($request->id);
 
-    $registration->score->update($request->only(['scores','comment']));
+		$registration->score->update($request->only(['scores','comment']));
 
-    return response()->json($registration, 200);
-  }
+		return response()->json($registration, 200);
+	}
 
-  /**
-   * Delete registrations
-   *
-   * @return void
-   */
-  public function delete(DeleteRegistration $request){
-    Registration::destroy($request->registration_ids);
+	/**
+	 * Delete registrations
+	 *
+	 * @return void
+	 */
+	public function delete(DeleteRegistration $request){
+		Registration::destroy($request->registration_ids);
 
-    return response()->json(true, 200);
-  }
+		return response()->json(true, 200);
+	}
 }
