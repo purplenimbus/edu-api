713279ac3edf6e7a115dca785aa0ba819b74fa97
Anthony Akpan <anthony.akpan@hotmail.com>
2020-11-03 23:32:44 -0800

    convert to spaces

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -13,67 +13,67 @@ use App\User;
 
 class RegisterController extends Controller
 {
-	/*
-	|--------------------------------------------------------------------------
-	| Register Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users as well as their
-	| validation and creation. By default this controller uses a trait to
-	| provide this functionality without requiring any additional code.
-	|
-	*/
+  /*
+  |--------------------------------------------------------------------------
+  | Register Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles the registration of new users as well as their
+  | validation and creation. By default this controller uses a trait to
+  | provide this functionality without requiring any additional code.
+  |
+  */
 
-	use RegistersUsers;
+  use RegistersUsers;
 
-	/**
-	 * Where to redirect users after registration.
-	 *
-	 * @var string
-	 */
-	protected $redirectTo = '/home';
+  /**
+   * Where to redirect users after registration.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/home';
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest');
+  }
 
-	/**
-	 * Create a new user instance after a valid registration.
-	 *
-	 * @param  array  $data
-	 * @return \App\User
-	 */
-	protected function create(StoreTenant $request)
-	{
-		$tenant = Tenant::create($request->only('name'));
-		$request = $this->parse_user($request);
-		$payload = $request->only(['firstname','lastname','email','password']);
-		$payload["tenant_id"] = $tenant->id;
-		$user = User::create($payload);
+  /**
+   * Create a new user instance after a valid registration.
+   *
+   * @param  array  $data
+   * @return \App\User
+   */
+  protected function create(StoreTenant $request)
+  {
+    $tenant = Tenant::create($request->only('name'));
+    $request = $this->parse_user($request);
+    $payload = $request->only(['firstname','lastname','email','password']);
+    $payload["tenant_id"] = $tenant->id;
+    $user = User::create($payload);
 
-		$tenant->setOwner($user);
-		$tenant->owner->notify(new ActivateTenant);
+    $tenant->setOwner($user);
+    $tenant->owner->notify(new ActivateTenant);
 
-		$tenant->createPayStackCustomer();
+    $tenant->createPayStackCustomer();
 
-		return response([
-			'message' => 'Account created, check your email to activate your account',
-		], 200);
-	}
+    return response([
+      'message' => 'Account created, check your email to activate your account',
+    ], 200);
+  }
 
-	private function parse_user(Request $request){
-		$data = [];
-		$fullName = explode(' ', $request->fullName);
-		$data['firstname'] = $fullName[0];
-		$data['lastname'] = isset($fullName[1]) ? $fullName[1] : '';
-		$request->merge($data);
+  private function parse_user(Request $request){
+    $data = [];
+    $fullName = explode(' ', $request->fullName);
+    $data['firstname'] = $fullName[0];
+    $data['lastname'] = isset($fullName[1]) ? $fullName[1] : '';
+    $request->merge($data);
 
-		return $request;
-	}
+    return $request;
+  }
 }
