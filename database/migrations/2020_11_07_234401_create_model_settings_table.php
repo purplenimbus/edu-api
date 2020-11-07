d8eb224bc6e5b76f01b0967cecc47a32a5ecaba5
Anthony Akpan <anthony.akpan@hotmail.com>
2020-11-07 15:56:23 -0800

    fix formatting

--- database/migrations/2020_11_07_234401_create_model_settings_table.php
+++ database/migrations/2020_11_07_234401_create_model_settings_table.php
@@ -6,23 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateModelSettingsTable extends Migration
 {
-    public function up()
-    {
-        if (!Schema::hasTable('model_settings')) {
-            Schema::create('model_settings', function (Blueprint $table) {
-                $table->increments('id');
-                $table->bigInteger('model_id');
-                $table->string('model_type');
-                $table->json('settings');
-                $table->timestamps();
+  public function up()
+  {
+    if (!Schema::hasTable('model_settings')) {
+      Schema::create('model_settings', function (Blueprint $table) {
+        $table->increments('id');
+        $table->bigInteger('model_id');
+        $table->string('model_type');
+        $table->json('settings');
+        $table->timestamps();
 
-                $table->unique(['model_id', 'model_type']);
-            });
-        }
+        $table->unique(['model_id', 'model_type']);
+      });
     }
+  }
 
-    public function down()
-    {
-        Schema::dropIfExists('model_settings');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('model_settings');
+  }
 }
