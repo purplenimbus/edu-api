182127fb836aaf145bde7f6d8639d08d5023fc7c
Oshioke Bello <bellozeekar@gmail.com>
2020-11-07 17:12:05 -0800

    Create checksubscription middleware and applied to requested routes.

--- app/Http/Middleware/HasBankAccount.php
+++ app/Http/Middleware/HasBankAccount.php
@@ -10,20 +10,20 @@ use Illuminate\Auth\Access\AuthorizationException;
 
 class HasBankAccount
 {
-		/**
-		 * Handle an incoming request.
-		 *
-		 * @param  \Illuminate\Http\Request  $request
-		 * @param  \Closure  $next
-		 * @return mixe
-		 */
-		public function handle($request, Closure $next)
-		{
-			$user = Auth::user();
-			$tenant = $user->tenant()->first();
-			if (!$tenant->has_bank_account){
-				throw new AuthorizationException(__('validation.custom.bank_account.not_set'));
-			}
-			return $next($request);
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixe
+	 */
+	public function handle($request, Closure $next)
+	{
+		$user = Auth::user();
+		$tenant = $user->tenant()->first();
+		if (!$tenant->has_bank_account){
+			throw new AuthorizationException(__('validation.custom.bank_account.not_set'));
 		}
+		return $next($request);
+	}
 }
