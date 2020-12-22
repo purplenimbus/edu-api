8224b42c20cf40f2de9679506bfb3e82d617fd65
Oshioke Bello <bellozeekar@gmail.com>
2020-12-22 12:31:41 -0800

    Fixed indentation

--- app/Observers/CourseObserver.php
+++ app/Observers/CourseObserver.php
@@ -6,87 +6,91 @@ use App\Course;
 
 class CourseObserver
 {
-	/**
-	 * Handle the course "created" event.
-	 *
-	 * @param  \App\Course  $course
-	 * @return void
-	 */
-	public function created(Course $course)
-	{
-		$this->setDefaultAttributes($course);
+  /**
+   * Handle the course "created" event.
+   *
+   * @param  \App\Course  $course
+   * @return void
+   */
+  public function created(Course $course)
+  {
+    $this->setDefaultAttributes($course);
 
-		$this->setNewInstructor($course);
-	}
+    $this->setNewInstructor($course);
+  }
 
-	/**
-	 * Handle the course "updated" event.
-	 *
-	 * @param  \App\Course  $course
-	 * @return void
-	 */
-	public function updated(Course $course)
-	{
-		$this->updateCourseAttributes($course);
-	}
+  /**
+   * Handle the course "updated" event.
+   *
+   * @param  \App\Course  $course
+   * @return void
+   */
+  public function updated(Course $course)
+  {
+    $this->updateCourseAttributes($course);
+  }
 
-	/**
-	 * Handle the course "deleted" event.
-	 *
-	 * @param  \App\Course  $course
-	 * @return void
-	 */
-	public function deleted(Course $course)
-	{
-		$this->deleteRegisteredStudents($course);
-	}
+  /**
+   * Handle the course "deleted" event.
+   *
+   * @param  \App\Course  $course
+   * @return void
+   */
+  public function deleted(Course $course)
+  {
+    $this->deleteRegisteredStudents($course);
+  }
 
-	private function setDefaultAttributes(Course $course) {
-		dd($course->grade->name);
-		if (is_null($course->name)) {
-			$course->name = $course->subject->name;
-		}
+  private function setDefaultAttributes(Course $course)
+  {
+    dd($course->grade->name);
+    if (is_null($course->name)) {
+      $course->name = $course->subject->name;
+    }
 
-		if (is_null($course->code)) {
-			$course->code = $course->parse_course_code();
-		}
-		
-		if (is_null($course->schema)) {
-			$course->schema = config('edu.default.course_schema');
-		}
-	}
+    if (is_null($course->code)) {
+      $course->code = $course->parse_course_code();
+    }
 
-	private function setNewInstructor(Course $course) {
-		if (request()->has('instructor_id') && $course->wasChanged('instructor_id')) {
-			$course->instructor->assignInstructor($course);
-		};
-	}
+    if (is_null($course->schema)) {
+      $course->schema = config('edu.default.course_schema');
+    }
+  }
 
-	private function updateCourseAttributes(Course $course) {
-		if (request()->has('instructor_id') && $course->wasChanged('instructor_id') && isset($course->instructor_id)) {
-			$course->instructor->assignInstructor($course);
-		}
+  private function setNewInstructor(Course $course)
+  {
+    if (request()->has('instructor_id') && $course->wasChanged('instructor_id')) {
+      $course->instructor->assignInstructor($course);
+    };
+  }
 
-		$this->updateCourseStatus($course);
-	}
+  private function updateCourseAttributes(Course $course)
+  {
+    if (request()->has('instructor_id') && $course->wasChanged('instructor_id') && isset($course->instructor_id)) {
+      $course->instructor->assignInstructor($course);
+    }
 
-	private function updateCourseStatus(Course $course) {
-		if ($course->status->name == 'complete')
-		{
-			$courses_in_progres = $course->where([
-				['tenant_id', '=', $course->tenant->id],
-				['status_id', '=', 1],
-			]);
+    $this->updateCourseStatus($course);
+  }
 
-			if ($courses_in_progres->count() == 0 && isset($course->tenant->current_term)){
-				$course->tenant->current_term->update(['status_id'=> 2]);
-			}
-		}
-	}
+  private function updateCourseStatus(Course $course)
+  {
+    if ($course->status->name == 'complete') {
+      $courses_in_progres = $course->where([
+        ['tenant_id', '=', $course->tenant->id],
+        ['status_id', '=', 1],
+      ]);
 
-	private function deleteRegisteredStudents(Course $course) {
-		if ($course->registrations()->count() > 0) {
-			$course->registrations()->delete();
-		}
-	}
+      if ($courses_in_progres->count() == 0 && isset($course->tenant->current_term)) {
+        $course->tenant->current_term->update(['status_id' => 2]);
+      }
+    }
+  }
+
+  private function deleteRegisteredStudents(Course $course)
+  {
+    if ($course->registrations()->count() > 0) {
+      $course->registrations()->delete();
+    }
+  }
 }
