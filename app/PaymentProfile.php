924b8a0b5d8bb492ff5abbd849edfa75eb36d6db
Anthony Akpan <anthony.akpan@hotmail.com>
2021-01-10 10:37:35 -0800

    fix indentation

--- app/PaymentProfile.php
+++ app/PaymentProfile.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentProfile extends Model
 {
-	/**
-	* The attributes that are mass assignable.
-	*
-	* @var array
-	*/
-	protected $fillable = [
-		'name',
-		'description',
-		'tenant_id',
-	];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name',
+    'description',
+    'tenant_id',
+  ];
 
-	public function PaymentItems(){
+  public function PaymentItems()
+  {
     return $this->hasMany('App\PaymentItem');
   }
 }
