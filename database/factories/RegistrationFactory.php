3e4597553291c2b51f787b7930669edb552d4d9b
Anthony Akpan <anthony.akpan@hotmail.com>
2021-01-14 15:09:38 -0800

    clean up registration factory

--- database/factories/RegistrationFactory.php
+++ database/factories/RegistrationFactory.php
@@ -2,11 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Course;
 use App\Registration;
+use App\Student;
+use App\Tenant;
 use Faker\Generator as Faker;
 
 $factory->define(Registration::class, function (Faker $faker) {
-    return [
-        //
-    ];
+  return [
+    'course_id' => factory(Course::class),
+    'tenant_id' => factory(Tenant::class),
+    'user_id' => factory(Student::class),
+  ];
 });
