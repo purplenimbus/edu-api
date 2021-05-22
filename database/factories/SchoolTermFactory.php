b255dfb06137b81a2cbe0f56a87e265a535cbc03
Anthony Akpan <anthony.akpan@hotmail.com>
2021-05-22 15:53:22 -0600

    fix indentation and add new lines

--- database/factories/SchoolTermFactory.php
+++ database/factories/SchoolTermFactory.php
@@ -10,13 +10,13 @@ use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(SchoolTerm::class, function (Faker $faker) {
-	return [
-		'description' => $faker->text(200),
+  return [
+    'description' => $faker->text(200),
     'end_date' => Carbon::now()->addMonths(4),
     'name' => $faker->text(200),
     'start_date' => Carbon::now(),
     'status_id' => factory(SchoolTermStatus::class),
     'tenant_id' => factory(Tenant::class),
     'type_id' => factory(SchoolTermType::class),
-	];
+  ];
 });
