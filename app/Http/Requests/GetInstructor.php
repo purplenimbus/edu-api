e97d62d3f2561cc4a832e0b8caa785a7850573dc
Anthony Akpan <anthony.akpan@hotmail.com>
2022-01-09 14:11:46 -0800

    fix indentation

--- app/Http/Requests/GetInstructor.php
+++ app/Http/Requests/GetInstructor.php
@@ -7,29 +7,30 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class GetInstructor extends FormRequest
 {
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
   {
     return [
       'id' => ['exists:users,id', new ValidInstructor()]
     ];
   }
 
-  public function validationData() {
+  public function validationData()
+  {
     return array_merge($this->all(), $this->route()->parameters());
   }
 }
--- app/Notifications/ActivateUser.php
+++ app/Notifications/ActivateUser.php
@@ -9,66 +9,66 @@ use Illuminate\Support\HtmlString;
 
 class ActivateUser extends VerifyEmail
 {
-	use Queueable;
+  use Queueable;
 
-	/**
-	 * Create a new notification instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		//
-	}
+  /**
+   * Create a new notification instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    //
+  }
 
-	/**
-	 * Get the notification's delivery channels.
-	 *
-	 * @param  mixed  $notifiable
-	 * @return array
-	 */
-	public function via($notifiable)
-	{
-		return ['mail'];
-	}
+  /**
+   * Get the notification's delivery channels.
+   *
+   * @param  mixed  $notifiable
+   * @return array
+   */
+  public function via($notifiable)
+  {
+    return ['mail'];
+  }
 
-	/**
-	 * Get the mail representation of the notification.
-	 *
-	 * @param  mixed  $notifiable
-	 * @return \Illuminate\Notifications\Messages\MailMessage
-	 */
-	public function toMail($notifiable)
-	{
-		if (static::$toMailCallback) {
-			return call_user_func(static::$toMailCallback, $notifiable);
-		}
+  /**
+   * Get the mail representation of the notification.
+   *
+   * @param  mixed  $notifiable
+   * @return \Illuminate\Notifications\Messages\MailMessage
+   */
+  public function toMail($notifiable)
+  {
+    if (static::$toMailCallback) {
+      return call_user_func(static::$toMailCallback, $notifiable);
+    }
 
-		$first_name = ucfirst($notifiable->firstname);
+    $first_name = ucfirst($notifiable->firstname);
 
-		$email = ucfirst($notifiable->email);
+    $email = ucfirst($notifiable->email);
 
     return (new MailMessage)
       ->subject(__('registration.welcome', ['name' => config('app.name')]))
-			->greeting(__('registration.hi', [ 'first_name' => $first_name ]))
-			->line(new HtmlString(__('registration.invited', [
-				'school_owner' => $notifiable->tenant->owner->fullname ?? '',
-				'school_name' => $notifiable->tenant->name ?? ''
-			])))
-			->line(__('registration.confirm'))
-			->action(__('registration.email', [ 'email' => strtolower($email) ]), $this->verificationUrl($notifiable));
-	}
+      ->greeting(__('registration.hi', ['first_name' => $first_name]))
+      ->line(new HtmlString(__('registration.invited', [
+        'school_owner' => $notifiable->tenant->owner->fullname ?? '',
+        'school_name' => $notifiable->tenant->name ?? ''
+      ])))
+      ->line(__('registration.confirm'))
+      ->action(__('registration.email', ['email' => strtolower($email)]), $this->verificationUrl($notifiable));
+  }
 
-	/**
-	 * Get the array representation of the notification.
-	 *
-	 * @param  mixed  $notifiable
-	 * @return array
-	 */
-	public function toArray($notifiable)
-	{
-		return [
-			//
-		];
-	}
+  /**
+   * Get the array representation of the notification.
+   *
+   * @param  mixed  $notifiable
+   * @return array
+   */
+  public function toArray($notifiable)
+  {
+    return [
+      //
+    ];
+  }
 }
