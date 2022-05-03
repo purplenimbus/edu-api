af7bb2b92d80897c736c82ee2938ba356c74c12e
Peace Sandy <78281826+Peacesandy@users.noreply.github.com>
2022-05-03 14:49:13 -0700

    Update registration.php

--- resources/lang/en/registration.php
+++ resources/lang/en/registration.php
@@ -3,17 +3,17 @@
 return [
   'account_created' => 'Account Created',
   'before' => 'but before you do that, you will need to confirm your email address',
-  'email' => 'yes :email is my email address',
+  'email' => 'yes :Kindly confirm your email',
   'email_resend' => 'Dont worry, we have resent the email confirmation',
   'email_success' => '<strong>:email</strong> successfully verified',
   'hi' => 'Hi :first_name!',
-  'login' => 'Login',
-  'one_step' => 'You are now one step to owning a leading school management system.',
-  'thanks' => 'thanks for signing up, you are obviously a person with high taste, welcome to a world of seamless learning. Login below to begin setting up your profile. <strong>:name</strong>',
+  'hope' => 'I hope you are having a wonderful day',
+  'one_step' => 'I hope you make the most out of this experience.',
+  'profile' => ' Please do well to set up your profile on the platform so you can start learning. <strong>:name</strong>',
   'token_expired' => 'Token expired',
-  'invited' => 'You have been invited by <strong>:school_owner</strong>, to join <strong>:school_name</strong>',
+  'invited' => ' This is to inform You that you have been invited by <strong>:Ekama Akpan</strong>, to join <strong>:Nimbus Grammar School</strong>',
   'confirm' => 'confirm your email below to begin setting up your profile',
   'welcome' => 'Welcome to :name',
-  'user_exist' => 'User dosent exist',
+  'Best Regards' => 'Best Regards',
   'verification_success' => 'Email verified!'
 ];
